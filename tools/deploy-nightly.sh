d3c70d190518a9c83c89be2696a035a0b7da3e62
Jared Boone <jboone@earfeast.com>
2019-03-27 15:31:09 -0700

    Travis-CI: Rework CI release artifact output.

---
+++ tools/deploy-nightly.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+PROJECT_NAME="PortaPack Nightly"
+PUBLICATION_BRANCH=master
+# set -x
+cd $HOME
+# Checkout the branch
+git clone --branch=$PUBLICATION_BRANCH https://${GITHUB_TOKEN}@github.com/${ARTEFACT_REPO}.git publish
+cd publish
+# Update pages
+cp $ARTEFACT_BASE/$BUILD_NAME.tar.xz .
+# Write index page
+cd $TRAVIS_BUILD_DIR
+COMMITS=`git log --oneline | awk '{print $1}'`
+cd $HOME/publish
+echo "
+<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">
+<html><head>
+	<meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">
+	<title>$PROJECT_NAME Builds</title>
+</head>
+<body>
+<h2>$PROJECT_NAME Builds</h2>
+" > index.html
+
+for commit in $COMMITS; do
+    FILENAME=`find . -maxdepth 1  -name "*-$commit.tar.xz"`
+    if [ "$FILENAME" != "" ]; then
+        FN=${FILENAME:2}
+        echo "<a href=\"${ARTEFACT_URL}/$FN\">$FN</a><br />" >> index.html
+    fi
+    
+done
+
+echo "
+</body></html>
+" >> index.html
+
+# Commit and push latest version
+git add $BUILD_NAME.tar.xz index.html
+git config user.name  "Travis"
+git config user.email "travis@travis-ci.org"
+git commit -m "Build products for $SHORT_COMMIT_HASH, built on $TRAVIS_OS_NAME, log: $TRAVIS_BUILD_WEB_URL"
+if [ "$?" != "0" ]; then
+    echo "Looks like the commit failed"
+fi
+git push -fq origin $PUBLICATION_BRANCH
