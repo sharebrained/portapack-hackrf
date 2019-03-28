72fdf7364fba15e8f0351f8e6996cbd9cf51d880
Jared Boone <jboone@earfeast.com>
2019-03-27 22:34:40 -0700

    Travis-CI: Introduce build directories, include MD5 and SHA256 hashes.

--- tools/deploy-nightly.sh
+++ tools/deploy-nightly.sh
@@ -6,7 +6,11 @@ cd $HOME
 git clone --branch=$PUBLICATION_BRANCH https://${GITHUB_TOKEN}@github.com/${ARTEFACT_REPO}.git publish
 cd publish
 # Update pages
-cp $ARTEFACT_BASE/$BUILD_NAME.tar.xz .
+BUILD_PATH=$BUILD_DATE-$SHORT_COMMIT_HASH
+mkdir $BUILD_PATH
+cp $ARTEFACT_BASE/$BUILD_NAME.tar.xz $BUILD_PATH/
+cp $ARTEFACT_BASE/MD5SUMS $BUILD_PATH/
+cp $ARTEFACT_BASE/SHA256SUMS $BUILD_PATH/
 # Write index page
 cd $TRAVIS_BUILD_DIR
 COMMITS=`git log --oneline | awk '{print $1}'`
@@ -19,27 +23,35 @@ echo "
 </head>
 <body>
 <h2>$PROJECT_NAME Builds</h2>
+<table>
 " > index.html
 
 for commit in $COMMITS; do
-    FILENAME=`find . -maxdepth 1  -name "*-$commit.tar.xz"`
-    if [ "$FILENAME" != "" ]; then
-        FN=${FILENAME:2}
-        echo "<a href=\"${ARTEFACT_URL}/$FN\">$FN</a><br />" >> index.html
-    fi
-    
+	FILEPATH=`find . -maxdepth 2 -name "*-$commit.tar.xz"`
+	if [ "$FILEPATH" != "" ]; then
+		FILEDIR=`dirname "${FILEPATH}"`
+		FILENAME=`basename "${FILENAME}"`
+		FILEPATH=${FILEPATH:2}
+		# pushd "${FILEDIR}" 
+		# HASH_MD5=`md5sum --binary ${FILENAME}`
+		# HASH_SHA256=`sha256sum --binary ${FILENAME}`
+		# popd
+		echo "<tr><td><a href=\"$FILEPATH\">$FILENAME</a></td><td><a href=\"$FILEDIR/MD5SUMS\">MD5SUMS</a></td><td><a href=\"$FILEDIR/SHA256SUMS\">SHA256SUMS</a></td></tr>" >> index.html
+	fi
+	
 done
 
 echo "
+</table>
 </body></html>
 " >> index.html
 
 # Commit and push latest version
-git add $BUILD_NAME.tar.xz index.html
+git add $BUILD_PATH/$BUILD_NAME.tar.xz $BUILD_PATH/MD5SUMS $BUILD_PATH/SHA256SUMS index.html
 git config user.name  "Travis"
 git config user.email "travis@travis-ci.org"
 git commit -m "Build products for $SHORT_COMMIT_HASH, built on $TRAVIS_OS_NAME, log: $TRAVIS_BUILD_WEB_URL"
 if [ "$?" != "0" ]; then
-    echo "Looks like the commit failed"
+	echo "Looks like the commit failed"
 fi
 git push -fq origin $PUBLICATION_BRANCH
